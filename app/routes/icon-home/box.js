0ec388969ae6c423c39d020e1c8d93901e54005c
Kayalvizhi Kathirvel <Kayalvizhi.K6@cognizant.com>
2017-06-05 16:17:15 +0530

    Refined code style

--- app/routes/icon-home/box.js
+++ app/routes/icon-home/box.js
@@ -2,15 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 model(params){
-let content=this.modelFor('iconHome')
-let boxContent={};  
-content.forEach(function(item) {
-  if(params.box_id===item.id)
-    {
-      boxContent=item;
-    }
-});
-return boxContent;
+  let content=this.modelFor('iconHome')
+  let boxContent=content.findBy("id",params.box_id)||{};
+  return boxContent;
 }
-
 });
